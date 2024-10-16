5b7c2eff69f576e784f5c4bda1d3a61fe6c232c4
hakureiyoru <768754163@qq.com>
2024-10-16 16:24:58 +0800

    Initial

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel-slide");
+
+function changeSlide(n) {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + n + slides.length) % slides.length;
+    slides[currentSlide].classList.add("active");
+}
+
+// 自动轮播
+let autoSlideInterval = setInterval(() => changeSlide(1), 5000); // 每5秒切换一次
+
+// 停止轮播时，重置自动轮播
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    autoSlideInterval = setInterval(() => changeSlide(1), 5000);
+}
